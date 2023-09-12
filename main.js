0ab026b10a6578309ed5cedd0249b392963eed8a
Paul Theby <paultheby@gmail.com>
2023-09-12 17:01:18 -0500

    Exercise 2.3
    I was able to bind the method from object 1 to object 2 and invoke it with the new functon at the end.

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ class Car {
         this. year = year
     }
     displayInfo() {
-        console.log(` ${this.year} ${this.make} ${this.model}`);
+        console.log(` ${this.year} ${this.make} ${this.model}`);//2013 Toyota 4Runner
     }
 }
 const newCar= new Car ("Toyota", "4Runner", 2013);
@@ -21,27 +21,39 @@ newCar.displayInfo();
 // Exercise 2.1: Craft a regular function within an object, then invoke it. Observe the behavior of "this". --not sure i did this one corectly invoking in console log outside of it. using myObject
 
 
-// const myObject = {
-//     firstName: 'Paul',
-//     age: 37,
-//     agePlus: function () {
-//     return this.age + 100
-//     }
-// };
+const exObject = {
+    firstName: 'Paul',
+    age: 37,
+    agePlus: function () {
+    return this.age + 100
+    }
+};
     
- 
-   
-// console.log(`hello, my name is ${myObject.firstName} and I will be ${myObject.agePlus()} in 100 years!`)
+ console.log(`hello, my name is ${exObject.firstName} and I will be ${exObject.agePlus()} in 100 years!`)
+ //Here i created an object called exObject with properties of a firstName, age, and the agePlus function
+ //that takes this.age and adds 100 and returns that when run. Then outside the object i console log the object and the objects function
 
-// // Exercise 2.2: Create an arrow function within another object. Execute it and note the behavior of "this".
+
+// // Exercise 2.2: Create an arrow function within another object. Execute it and note the behavior of "this"
+//not sure how to do this one. 
 
 // const objectTwo= {
-//     lastName: "Theby"
-//     sayHello: hello()=> {
-//         return console.log(`Hello! + ${this.lastName}`)
+//     lastName: "Theby",
+//     sayHello: ()=> {
+//         console.log(`Hello! + ${this.lastName}`)   //"this does not work here because of arrow functions not inheriting the object    
 //     }
+//     };
+//    objectTwo.sayHello();   //RESULT: HELLO UNDEFINED. THIS IS UNDEFINED!
+
    
-// };
+// const objectTwo= {
+//     lastName: "Theby",
+//     sayHello: ()=> {
+//         console.log(`Hello!  ${objectTwo.lastName}`)   //"this does not work here because of arrow functions not inheriting the object    }
+//     }
+//     };
+//    objectTwo.sayHello();
+//    ////i had to not use the actual object name for it to log 
 
 
 
@@ -49,3 +61,23 @@ newCar.displayInfo();
 
 
 // Exercise 2.3: Using the .bind() method, manually set the context of a function and observe the change in the behavior of "this".
+
+//copying function from above and then binding a new object to it?
+
+const objectTwo= {
+    lastName: "Theby",
+    sayHello() {
+        console.log(`Hello! + ${this.lastName}`)  
+    }
+    };
+const objectThree = {
+    lastName: "Smith",
+
+};
+objectTwo.sayHello();
+
+const sayHelloAgain= objectTwo.sayHello.bind(objectThree);
+sayHelloAgain(); //Hello + Smith
+
+//here i take the original object that has a method in it and then create another object with a same parameter like "lastName". i then create another object that is labeled similiary to the method1 in object 1 but i 
+// attached that object.method.bind(context)
